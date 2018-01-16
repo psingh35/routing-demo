60b48b4022f8787b372135dcd670fcf45b95e12f
Parminder Singh <psingh35@mail.greenriver.edu>
2018-01-16 16:23:57 -0500

    added toering

--- index.php
+++ index.php
@@ -8,12 +8,27 @@ $f3 = Base :: instance();
 //Define a default rout
 $f3->route('GET /', function ()
 {
-    $view = new View;
-    echo $view->render
-        echo '<h1>Routing Demo</h1>';
-
+    echo '<h1>Hello1</h1>';
 }
 );
+
+//Define a default rout
+$f3->route('GET /page1/subpage-a', function ()
+{
+        echo '<h1>This is page 1, Subpage !</h1>';
+}
+);
+//Define a default rout
+$f3->route('GET /jewelry/rings/toe-rings', function ()
+{
+    $template = mew Template = new Template();
+    echo $template ->render ('views/Jewelry.html');
+}
+);
+
+//set debug level
+    $f3->set('DEBUG', 3);
+
 
 //Run fat free
 $f3->run();
